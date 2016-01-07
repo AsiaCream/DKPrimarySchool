13ad975fcb98dbd083d20382987210529093f058
Cream2015 <343224963@qq.com>
2016-01-08 01:05:48 +0800

    新增加具体文章页面

---
+++ WebSchool2012023015/Web2012023015School/src/Web2012023015School/Controllers/ShowController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Web2012023015School.Models;
+
+namespace Web2012023015School.Controllers
+{
+    public class ShowController : BaseController
+    {
+        [FromServices]
+        public ArticleContext DB { get; set; }
+        // GET: /<controller>/
+        public IActionResult Article(int id)
+        {
+            var article = DB.Article.Where(x=>x.Id==id).SingleOrDefault();          
+            return View(article);
+        }
+        public IActionResult Inform()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Photes()
+        {
+            return View();
+        }
+        public IActionResult RecruitStudents()
+        {
+            return View();
+        }
+    }
+}
